1502c4503bb68017d4a40b3b7f061eaa2a33e64d
FoxxoTrystan <trystan.garnierhein@gmail.com>
2024-12-09 00:52:55 +0100

    voresound local

--- Content.Server/FloofStation/VoreSystem.cs
+++ Content.Server/FloofStation/VoreSystem.cs
@@ -91,7 +91,7 @@ public sealed class VoreSystem : EntitySystem
         if (!args.CanInteract
             || !args.CanAccess
             || args.User == args.Target
-            || !HasComp<MobStateComponent>(args.Target)
+            || !HasComp<VoreComponent>(args.Target)
             || !_consent.HasConsent(args.Target, "Vore")
             || HasComp<VoredComponent>(args.User))
             return;
@@ -212,7 +212,26 @@ public sealed class VoreSystem : EntitySystem
         _containerSystem.Insert(target, component.Stomach);
         _audioSystem.PlayPvs(component.SoundDevour, uid);
 
-        _popups.PopupEntity(Loc.GetString("vore-devoured", ("entity", uid), ("prey", target)), uid, PopupType.LargeCaution);
+        if (_playerManager.TryGetSessionByEntity(target, out var sessionprey)
+            || sessionprey is not null)
+            _audioSystem.PlayEntity(component.SoundDevour, sessionprey, uid);
+
+        if (_playerManager.TryGetSessionByEntity(uid, out var sessionpred)
+            || sessionpred is not null)
+        {
+            _audioSystem.PlayEntity(component.SoundDevour, sessionpred, uid);
+            // var message = Loc.GetString("", ("entity", uid));
+            // _chatManager.ChatMessageToOne(
+            //     ChatChannel.Emotes,
+            //     message,
+            //     message,
+            //     EntityUid.Invalid,
+            //     false,
+            //     sessionprey.Channel);
+        }
+
+        _popups.PopupEntity(Loc.GetString("vore-devoured", ("entity", uid), ("prey", target)), target, target, PopupType.SmallCaution);
+        _popups.PopupEntity(Loc.GetString("vore-devoured", ("entity", uid), ("prey", target)), target, uid, PopupType.SmallCaution);
 
         _adminLog.Add(LogType.Action, LogImpact.High, $"{ToPrettyString(uid)} vored {ToPrettyString(target)}");
     }
@@ -232,9 +251,16 @@ public sealed class VoreSystem : EntitySystem
         if (TryComp<TemperatureComponent>(uid, out var temp))
             temp.AtmosTemperatureTransferEfficiency = 0.1f;
 
-        _audioSystem.PlayPvs(component.SoundRelease, args.Container.Owner);
+        if (_playerManager.TryGetSessionByEntity(args.Container.Owner, out var sessionpred)
+            || sessionpred is not null)
+            _audioSystem.PlayEntity(component.SoundRelease, sessionpred, uid);
 
-        _popups.PopupEntity(Loc.GetString("vore-released", ("entity", uid), ("pred", args.Container.Owner)), uid, PopupType.Large);
+        if (_playerManager.TryGetSessionByEntity(uid, out var sessionprey)
+            || sessionprey is not null)
+            _audioSystem.PlayEntity(component.SoundRelease, sessionprey, uid);
+
+        _popups.PopupEntity(Loc.GetString("vore-released", ("entity", uid), ("pred", args.Container.Owner)), uid, args.Container.Owner, PopupType.Medium);
+        _popups.PopupEntity(Loc.GetString("vore-released", ("entity", uid), ("pred", args.Container.Owner)), uid, uid, PopupType.Medium);
 
         _adminLog.Add(LogType.Action, LogImpact.Medium, $"{ToPrettyString(uid)} got released from {ToPrettyString(args.Container.Owner)} belly");
     }
