b5f1a76cc02339988b08053affb58906ba444759
Forzii <github@pawsitive.mozmail.com>
2025-03-11 06:15:49 -0400

    Add Floof Consent Menu (#154)
    * Merge pull request #4 from Fansana/ConsentMenu
    
    Added consent Menu
    
    * Add length CVar
    
    This was in the original PR but in the CCVars folder. Due to frontier's Cvar refactor this wasn't viable.
    
    * Fix CVar references
    
    * Merge pull request #373 from FoxxoTrystan/consent-fixes
    
    Consent UI Generated
    
    * Proper database migration
    
    * Vore Fixes and Consent Menu Improvement (#568)
    
    * vore fixes and consent menu improvement
    
    * consent menu fixes
    
    * Update Content.Shared/Consent/ConsentTogglePrototype.cs
    
    Co-authored-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
    
    * Category has a default of "" to pull uncategorized consents to the top (NoClone has been uncategorized as an example)
    ConsentTogglePrototype.CompareTo override modifier removed, does not compile with it
    System.Linq.Enumerable.ToList<T>() used instead of List constructor
    
    * I Understand Now
    
    ---------
    
    Co-authored-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
    
    * Additional comments and cleanup
    
    ---------
    
    Co-authored-by: Fansana <116083121+Fansana@users.noreply.github.com>
    Co-authored-by: LucasTheDrgn <kirbyfan.95@gmail.com>
    Co-authored-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>

--- Content.Client/Input/ContentContexts.cs
+++ Content.Client/Input/ContentContexts.cs
@@ -125,6 +125,7 @@ namespace Content.Client.Input
             common.AddFunction(ContentKeyFunctions.OpenSandboxWindow);
             common.AddFunction(ContentKeyFunctions.OpenTileSpawnWindow);
             common.AddFunction(ContentKeyFunctions.OpenDecalSpawnWindow);
+            common.AddFunction(ContentKeyFunctions.OpenConsentWindow); // Floofstation
             common.AddFunction(ContentKeyFunctions.OpenAdminMenu);
             common.AddFunction(ContentKeyFunctions.OpenGuidebook);
         }
--- Content.Shared.Database/LogType.cs
+++ Content.Shared.Database/LogType.cs
@@ -515,4 +515,5 @@ public enum LogType
     /// </summary>
     ObjectiveSummary = 422,
     #endregion DeltaV Values
+    Consent = 12489, // Floofstation - random high number to avoid conflict when upstream adds new log types
 }
