1a5e5758ff5048218dd92aa977687e56706920ca
Danny Kay <sprambersonson@gmail.com>
2025-07-06 22:42:03 -0700

    fixed rat undies

--- Content.Client/Humanoid/HumanoidAppearanceSystem.cs
+++ Content.Client/Humanoid/HumanoidAppearanceSystem.cs
@@ -237,11 +237,11 @@ public sealed class HumanoidAppearanceSystem : SharedHumanoidAppearanceSystem
         // Really, markings should probably be a separate component altogether.
         ClearAllMarkings(humanoid, sprite);
 
-        var censorNudity = _configurationManager.GetCVar(CCVars.AccessibilityClientCensorNudity) ||
-                           _configurationManager.GetCVar(CCVars.AccessibilityServerCensorNudity);
+        // var censorNudity = _configurationManager.GetCVar(CCVars.AccessibilityClientCensorNudity) ||
+        //                    _configurationManager.GetCVar(CCVars.AccessibilityServerCensorNudity);
         // The reason we're splitting this up is in case the character already has undergarment equipped in that slot.
-        var applyUndergarmentTop = censorNudity;
-        var applyUndergarmentBottom = censorNudity;
+        // var applyUndergarmentTop = censorNudity;
+        // var applyUndergarmentBottom = censorNudity;
 
         foreach (var markingList in humanoid.MarkingSet.Markings.Values)
         {
@@ -250,17 +250,17 @@ public sealed class HumanoidAppearanceSystem : SharedHumanoidAppearanceSystem
                 if (_markingManager.TryGetMarking(marking, out var markingPrototype))
                 {
                     ApplyMarking(markingPrototype, marking.MarkingColors, marking.Visible, humanoid, sprite);
-                    if (markingPrototype.BodyPart == HumanoidVisualLayers.UndergarmentTop)
-                        applyUndergarmentTop = false;
-                    else if (markingPrototype.BodyPart == HumanoidVisualLayers.UndergarmentBottom)
-                        applyUndergarmentBottom = false;
+                    // if (markingPrototype.BodyPart == HumanoidVisualLayers.UndergarmentTop)
+                    //     applyUndergarmentTop = false;
+                    // else if (markingPrototype.BodyPart == HumanoidVisualLayers.UndergarmentBottom)
+                    //     applyUndergarmentBottom = false;
                 }
             }
         }
 
         humanoid.ClientOldMarkings = new MarkingSet(humanoid.MarkingSet);
 
-        AddUndergarments(humanoid, sprite, applyUndergarmentTop, applyUndergarmentBottom);
+        // AddUndergarments(humanoid, sprite, applyUndergarmentTop, applyUndergarmentBottom);
     }
 
     private void ClearAllMarkings(HumanoidAppearanceComponent humanoid, SpriteComponent sprite)
