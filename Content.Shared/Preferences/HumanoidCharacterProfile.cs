748cfa7fd8b45d7f7a0f0daf626ab87f342377c2
Jaeger <80431951+Kilmented@users.noreply.github.com>
2025-07-23 19:12:57 -0500

    increases starting funds to prepare for the fallout

--- Content.Shared/Preferences/HumanoidCharacterProfile.cs
+++ Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -34,7 +34,7 @@ namespace Content.Shared.Preferences
         public const int MaxLoadoutNameLength = 32;
         public const int MaxDescLength = 4000; //maximum discord message length, without nitro
 
-        public const int DefaultBalance = 30000;
+        public const int DefaultBalance = 50000; // Frontier's Default bank balance = 30000
 
         //private readonly Dictionary<string, JobPriority> _jobPriorities; // Frontier: commented out during merge.
         //private readonly List<string> _antagPreferences; // Frontier: commented out during merge.
