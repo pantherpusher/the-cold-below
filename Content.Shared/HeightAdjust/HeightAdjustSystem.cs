ee5e29644762313180a946abd81215750752e347
Danny Kay <sprambersonson@gmail.com>
2025-08-10 17:45:51 -0700

    COOL FUCKING CVAR GUYS

--- Content.Shared/HeightAdjust/HeightAdjustSystem.cs
+++ Content.Shared/HeightAdjust/HeightAdjustSystem.cs
@@ -26,16 +26,16 @@ public sealed class HeightAdjustSystem : EntitySystem
     public bool SetScale(EntityUid uid, float scale)
     {
         var succeeded = true;
-        if (_config.GetCVar(CCVars.HeightAdjustModifiesZoom) && EntityManager.TryGetComponent<ContentEyeComponent>(uid, out var eye))
-            _eye.SetMaxZoom(uid, eye.MaxZoom * scale);
-        else
-            succeeded = false;
-
-        if (_config.GetCVar(CCVars.HeightAdjustModifiesHitbox) && EntityManager.TryGetComponent<FixturesComponent>(uid, out var fixtures))
-            foreach (var fixture in fixtures.Fixtures)
-                _physics.SetRadius(uid, fixture.Key, fixture.Value, fixture.Value.Shape, MathF.MinMagnitude(fixture.Value.Shape.Radius * scale, 0.49f));
-        else
-            succeeded = false;
+        // if (_config.GetCVar(CCVars.HeightAdjustModifiesZoom) && EntityManager.TryGetComponent<ContentEyeComponent>(uid, out var eye))
+        //     _eye.SetMaxZoom(uid, eye.MaxZoom * scale);
+        // else
+        //     succeeded = false;
+        //
+        // if (_config.GetCVar(CCVars.HeightAdjustModifiesHitbox) && EntityManager.TryGetComponent<FixturesComponent>(uid, out var fixtures))
+        //     foreach (var fixture in fixtures.Fixtures)
+        //         _physics.SetRadius(uid, fixture.Key, fixture.Value, fixture.Value.Shape, MathF.MinMagnitude(fixture.Value.Shape.Radius * scale, 0.49f));
+        // else
+        //     succeeded = false;
 
         if (EntityManager.HasComponent<HumanoidAppearanceComponent>(uid))
         {
@@ -59,16 +59,16 @@ public sealed class HeightAdjustSystem : EntitySystem
         var succeeded = true;
         var avg = (scale.X + scale.Y) / 2;
 
-        if (_config.GetCVar(CCVars.HeightAdjustModifiesZoom) && EntityManager.TryGetComponent<ContentEyeComponent>(uid, out var eye))
-            _eye.SetMaxZoom(uid, eye.MaxZoom * avg);
-        else
-            succeeded = false;
-
-        if (_config.GetCVar(CCVars.HeightAdjustModifiesHitbox) && EntityManager.TryGetComponent<FixturesComponent>(uid, out var fixtures))
-            foreach (var fixture in fixtures.Fixtures)
-                _physics.SetRadius(uid, fixture.Key, fixture.Value, fixture.Value.Shape, MathF.MinMagnitude(fixture.Value.Shape.Radius * avg, 0.49f));
-        else
-            succeeded = false;
+        // if (_config.GetCVar(CCVars.HeightAdjustModifiesZoom) && EntityManager.TryGetComponent<ContentEyeComponent>(uid, out var eye))
+        //     _eye.SetMaxZoom(uid, eye.MaxZoom * avg);
+        // else
+        //     succeeded = false;
+        //
+        // if (_config.GetCVar(CCVars.HeightAdjustModifiesHitbox) && EntityManager.TryGetComponent<FixturesComponent>(uid, out var fixtures))
+        //     foreach (var fixture in fixtures.Fixtures)
+        //         _physics.SetRadius(uid, fixture.Key, fixture.Value, fixture.Value.Shape, MathF.MinMagnitude(fixture.Value.Shape.Radius * avg, 0.49f));
+        // else
+        //     succeeded = false;
 
         if (EntityManager.HasComponent<HumanoidAppearanceComponent>(uid))
             _appearance.SetScale(uid, scale);
